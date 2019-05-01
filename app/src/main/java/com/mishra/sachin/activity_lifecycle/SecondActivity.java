48fbfcd5280ce9a0923b4f0cc4d192101fbc74c1
Sachin Mishra <sachinid16@gmail.com>
2019-05-01 23:33:32 +0530

    change in function name

--- app/src/main/java/com/mishra/sachin/activity_lifecycle/SecondActivity.java
+++ app/src/main/java/com/mishra/sachin/activity_lifecycle/SecondActivity.java
@@ -11,47 +11,47 @@ public class SecondActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        Toast.makeText(this, "2 oncreate", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2 oncreate()", Toast.LENGTH_SHORT).show();
     }
 
 
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "2 on start", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2 on start()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPostResume() {
         super.onPostResume();
 
-        Toast.makeText(this, "2 postresume", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2 postresume()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Toast.makeText(this, "2 restart", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2 restart()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
 
-        Toast.makeText(this, "2 pause ", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2 pause() ", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop() {
         super.onStop();
 
-        Toast.makeText(this, "2 onstop", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2 onstop()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
 
-        Toast.makeText(this, "2 on destroy", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "2 ondestroy()", Toast.LENGTH_SHORT).show();
     }
 }
